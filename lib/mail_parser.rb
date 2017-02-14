aa2b9509ea689989c2dafb3f3b9675b2fd6e728e
Rodrigo Haenggi <rodrigo@codegestalt.com>
2017-02-14 15:31:32 +0100

    stage 1

--- lib/mail_parser.rb
+++ lib/mail_parser.rb
@@ -8,11 +8,18 @@ class MailParser
 end
 
 class MailData
-  attr_reader :data, :date, :time_window, :customer, :street, :zip, :total
+  attr_reader :date, :time_window, :customer, :street, :zip, :total
 
   def initialize(body)
-    @data = clean(body).split("\n").map(&:strip)
-    set_attributes
+    data = clean(body).split("\n").map(&:strip)
+    element = DataElement.new(data)
+
+    @date                  = element.date
+    @time_window           = element.time_window
+    @customer              = element.customer
+    @street                = element.street
+    @zip                   = element.zip
+    @total                 = element.total
   end
 
   def to_h
@@ -22,20 +29,43 @@ class MailData
 
   private
 
-  def set_attributes
-    datetime_index = data.index('Kundeninformation') || data.index('Customer information')
-    zip_index = data.index{ |index| index.match(/^[8]\d{3}{1,4}$/) }
-    total_index = data.index('Total in CHF')
-
-    _, @date, @time_window = data.at(datetime_index - 1).split(' ')
-    @customer              = data.at(zip_index - 3)
-    @street                = data.at(zip_index - 1)
-    @zip                   = data.at(zip_index)
-    @total                 = data.at(total_index + 1)
-  end
-
   def clean(string)
     replacements = { '> ' => '', "\r" => '', '=' => '%' }
     CGI.unescape(string.gsub(Regexp.union(replacements.keys), replacements))
+  end
+end
+
+class DataElement
+  attr_reader :data, :customer_index, :price_index, :time_index
+
+  def initialize(array)
+    @data = array
+    @time_index = array.index('Kundeninformation') || array.index('Customer information')
+    @customer_index = array.index { |i| i.match(/^[8]\d{3}{1,4}$/) }
+    @price_index = array.index('Total in CHF')
+  end
+
+  def date
+    data.at(time_index - 1).split(' ')[1]
+  end
+
+  def time_window
+    data.at(time_index - 1).split(' ')[2]
+  end
+
+  def total
+    data.at(price_index + 1)
+  end
+
+  def customer
+    data.at(customer_index - 3)
+  end
+
+  def street
+    data.at(customer_index - 1)
+  end
+
+  def zip
+    data.at(customer_index)
   end
 end
