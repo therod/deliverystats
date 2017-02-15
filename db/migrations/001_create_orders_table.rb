8b588ba030c34c9a2c6e6807e049e112c27c0ab7
Rodrigo Haenggi <rodrigo@codegestalt.com>
2017-02-15 16:11:40 +0100

    use new types for database

--- db/migrations/001_create_orders_table.rb
+++ db/migrations/001_create_orders_table.rb
@@ -1,20 +1,15 @@
 require_relative '../../environment'
 
-class CreateOrdersTable < ActiveRecord::Migration
+class CreateOrdersTable < ActiveRecord::Migration[5.0]
   def up
     create_table :orders do |t|
       t.string :customer_name
       t.string :street
       t.string :zip
-
-      t.string :total
-      t.string :time_window
       t.date :date
-
-      # TODO Split up these fields
-      # t.float :total
-      # t.time :time_start
-      # t.time :time_end
+      t.float :total
+      t.time :time_start
+      t.time :time_end
     end
   end
 
